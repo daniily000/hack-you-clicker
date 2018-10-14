f8baff9dcf4e04cdc396d9e24183286670f05d91
daniily000 <daniily000@yandex.ru>
2018-10-14 19:11:25 +0200

    add project base, activity_menu.xml, MenuButton, basic styles

---
+++ app/src/main/java/com/daniily000/android/hackyou/view/MenuButton.kt
@@ -0,0 +1,38 @@
+package com.daniily000.android.hackyou.view
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.MotionEvent
+import com.daniily000.android.hackyou.R
+
+class MenuButton(context: Context, attrs: AttributeSet) : android.support.v7.widget.AppCompatTextView(context, attrs) {
+
+
+    var highlightingColor: Int
+    var defaultColor: Int
+
+    init {
+        val typedArray = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.MenuButton,
+                0,
+                0
+        )
+        highlightingColor = typedArray.getColor(R.styleable.MenuButton_highlightingColor, currentTextColor)
+        defaultColor = currentTextColor
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            setTextColor(highlightingColor)
+            return true
+        }
+
+        if (event?.action == MotionEvent.ACTION_UP) {
+            setTextColor(defaultColor)
+            return true
+        }
+        return false
+    }
+}
